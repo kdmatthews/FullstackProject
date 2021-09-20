eadeed060a7506b1bd1fe4647fd73a509df8a59e
pamela long <usmc.pnlong@gmail.com>
2021-09-20 15:57:54 -0400

    updated

--- database.js
+++ database.js
@@ -1,39 +1,62 @@
 const express = require("express");
 const app = express();
+// const url = "postgres://wxinvkdg:CGsVawzhAN_mf6ORkR_ABFzK8XJOyfN3@chunee.db.elephantsql.com/wxinvkdg"
+
+
 
 const creds = require("./db");
+console.log (creds);
 
-const PORT = 5432;
+const PORT = 3001;
 
 app.use(express.json());
 
 
 //CREATE
-app.post("/create_user", async (req,res) => {
-    const user_name = req.body.user_name
-    const password = req.body.password
-    creds.connect((err, client, release) => {
-        if (err) {
-            return console.error("Error getting connected to the client", err.stack);
-        }
-    
-    client.query`(INSERT INTO resturants (id,name,street, zipcode,city), VALUES ('${req.body.id}','${req.body.name}','${req.body.street}','${req.body.zipcode}','${req.body.city}')`, (err, result) => {
-        if (err) {
-            res.status(400).send(err.stack);
-        }
-            res.status(200).send(result.rows);
-        };
-    });
-
-    const new_user = await SeaWorld.create({
-        user_name: user_name,
-        password: password
-    });
+app.post("/create_user", (req,res) => {
+    try{
+        creds.connect(async() => {
+        const data = await creds.query(`INSERT INTO users(user_name, password) VALUES ('${req.body.user_name}','${req.body.password}')`)
+        res.send(`Inserted New user with ${req.body.user_name} ${req.body.password}`);    
+            });
+    }catch(err){
+res.send(err);
+    };
+});
+//Read
+app.put("/read_user", (req,res) => {
+    try{
+        creds.connect(async() => {
+        const data = await creds.query(`users(user_name, password) VALUES ('${req.body.user_name}','${req.body.password}')`)
+        res.send(`${req.body.user_name} ${req.body.password}`);    
+            });
+    }catch(err){
+res.send(err);
+    };
 });
 
+//UPDATE
+app.post("/create_user", (req,res) => {
+    try{
+        creds.connect(async() => {
+        const data = await creds.query(`INSERT INTO users(user_name, password) VALUES ('${req.body.user_name}','${req.body.password}')`)
+        res.send(`Inserted New user with ${req.body.user_name} ${req.body.password}`);    
+            });
+    }catch(err){
+res.send(err);
+    }
 
-
-// res.send(`Inserted New user with ${newUser.user_name} ${newUser.password}`);
-
+//DELETE
+app.post("/create_user", (req,res) => {
+    try{
+        creds.connect(async() => {
+        const data = await creds.query(`INSERT INTO users(user_name, password) VALUES ('${req.body.user_name}','${req.body.password}')`)
+        res.send(`Inserted New user with ${req.body.user_name} ${req.body.password}`);    
+            });
+    }catch(err){
+res.send(err);
+    };
+});
+});
 
 app.listen(PORT, console.log(`Listening on port ${PORT}`));
