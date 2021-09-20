c93fa87598f740c1b6c9f34c928babab4a885858
pamela long <usmc.pnlong@gmail.com>
2021-09-20 12:06:42 -0400

    updated

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const creds = require("./db.js");
+const creds = require("./db");
 
 const PORT = 5432;
 
@@ -12,6 +12,18 @@ app.use(express.json());
 app.post("/create_user", async (req,res) => {
     const user_name = req.body.user_name
     const password = req.body.password
+    creds.connect((err, client, release) => {
+        if (err) {
+            return console.error("Error getting connected to the client", err.stack);
+        }
+    
+    client.query`(INSERT INTO resturants (id,name,street, zipcode,city), VALUES ('${req.body.id}','${req.body.name}','${req.body.street}','${req.body.zipcode}','${req.body.city}')`, (err, result) => {
+        if (err) {
+            res.status(400).send(err.stack);
+        }
+            res.status(200).send(result.rows);
+        };
+    });
 
     const new_user = await SeaWorld.create({
         user_name: user_name,
