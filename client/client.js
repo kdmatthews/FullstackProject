2b674dcb5e1653fcc82baee101d0cee3d656d878
Kayla Matthews <kdmatthews77@gmail.com>
2021-09-21 16:45:40 -0400

    updated login fetch

--- client/client.js
+++ client/client.js
@@ -37,29 +37,29 @@ document.addEventListener("DOMContentLoaded", () => {
         loginForm.classList.remove('form-hidden')
 
     });
-    const loginUser = async () => {
-        const user_name = document.querySelector("#loginUsername").value
-        const password = document.querySelector("#loginPassword").value
-        const loginData = {
-            user_name,
-            password,
-        }
-        const readUserData = await fetch("http://localhost:3001/read_user", {
-            method: "GET",
-            mode: "cors",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(loginData),
-        }) 
-        console.log(readUserData.status);
-        console.log(readUserData.status.Text);
+    // const loginUser = async () => {
+    //     const user_name = document.querySelector("#loginUsername").value
+    //     const password = document.querySelector("#loginPassword").value
+    //     const loginData = {
+    //         user_name,
+    //         password,
+    //     }
+    //     const readUserData = await fetch("http://localhost:3001/read_user", {
+    //         method: "GET",
+    //         mode: "cors",
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify(loginData),
+    //     }) 
+    //     console.log(readUserData.status);
+    //     console.log(readUserData.status.Text);
 
-        if (response.status === 200) {
-            let data = await response.text();
-        }
+    //     if (response.status === 200) {
+    //         let data = await response.text();
+    //     }
        
-    }
+    
     loginForm.addEventListener("submit", e => {
         e.preventDefault();
        
