95d8b1e0454e9c622b4932bf706fa944fd388b09
pamela long <usmc.pnlong@gmail.com>
2021-09-21 16:45:49 -0400

    updated for rides

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-// const url = "postgres://wxinvkdg:CGsVawzhAN_mf6ORkR_ABFzK8XJOyfN3@chunee.db.elephantsql.com/wxinvkdg"
+// const url = "postgres://wxinvkdg:K7nLI4ZVLa3UfIeds01LON96wzevUCHD@chunee.db.elephantsql.com/wxinvkdg"
 
 
 
@@ -12,18 +12,18 @@ app.use(express.json());
 
 
 //CREATE: USER
-app.post("/create_user", (req,res) => {
-    creds.connect(async() => {
-    try{
-        const data = await creds.query(`INSERT INTO users(user_name, password) VALUES ('${req.body.user_name}','${req.body.password}')`)
-        res.send(`Inserted New user with ${req.body.user_name} ${req.body.password}`);
-        // console.log(res.send);
-    }catch(err){
-        res.send(err);
-    };
-});
+// app.post("/create_user", (req,res) => {
+//     creds.connect(async() => {
+//     try{
+//         const data = await creds.query(`INSERT INTO users(user_name, password) VALUES ('${req.body.user_name}','${req.body.password}')`)
+//         res.send(`Inserted New user with ${req.body.user_name} ${req.body.password}`);
+//         // console.log(res.send);
+//     }catch(err){
+//         res.send(err);
+//     };
+// });
     
-});
+// });
 
 //Read  SELECT * info: USER
 // app.get("/read_user", (req,res) => {
@@ -52,10 +52,11 @@ app.post("/create_user", (req,res) => {
 // });
 
 // //DELETE: USER
-// app.delete("/delete_user", (req,res) => {
+// app.delete("/delete_user/:id", (req,res) => {
+//     const id = req.params.id;
 //     try{
 //         creds.connect(async() => {
-//         const data = await creds.query(`DELETE FROM users(user_name, password) VALUES ('${req.body.user_name}','${req.body.password}')`)
+//         const data = await creds.query(`DELETE FROM users WHERE id=8`);
 //         res.send(`User has been deleted`);
 //         console.log(res.send);
 //             });
@@ -64,4 +65,19 @@ app.post("/create_user", (req,res) => {
 // };
 
 // });
+
+
+
+// Read  SELECT * info: RIDES
+app.get("/read_rides", (req,res) => {
+    try{
+        creds.connect(async() => {
+        const data = await creds.query(`SELECT * FROM rides`);
+        res.send (data);    
+            });
+    }catch(err){
+res.send(err);
+    };
+    
+});
 app.listen(PORT, console.log(`Listening on port ${PORT}`));
